c8f551891c651288f0f9c31e76267797f73ad4ca
Tony Dim <C:\Users\Tony.DESKTOP-HMJLVQ3\AppData\Roaming\The Bat! Pwd>
2023-10-08 13:17:59 -0400

    unpublish: conflict resolved

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 import useCatchErrors from '../error/catchErrors';
-import ProductController from '../controller/product.controller';
+import productController from '../controller/product.Controller';
 
 export default class ProductRoute {
   router = express.Router();
-  productController = new ProductController();
+  productController = new productController();
   path = '/products';
 
   constructor() {
@@ -14,11 +14,15 @@ export default class ProductRoute {
   initializeRoutes() {
     this.router.get(
       `${this.path}/publish/:productId`,
-      useCatchErrors(this.productController.publishProduct.bind(this.productController)),
+      useCatchErrors(this.productController.publishProduct.bind(this.productController))
     );
     this.router.post(
       `${this.path}/add`,
-      useCatchErrors(this.productController.addProduct.bind(this.productController)),
+      useCatchErrors(this.productController.addProduct.bind(this.productController))
+    );
+    this.router.post(
+      `${this.path}/:productId/draft`,
+      useCatchErrors(this.productController.addProductDraft.bind(this.productController))
     );
     this.router.patch(
       `${this.path}/unpublish/:productId`,
