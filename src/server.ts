ec0e0d399706cc186ce56644698d995d31991b1e
Benrobo <alumonabenaiah71@gmail.com>
2023-10-08 13:57:33 +0100

    feat: worked on the skelenton mvp test for my task

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
+import ProductRoute from 'routes/product.Route';
 import App from './app';
 import UserRoute from './routes/user.route';
 
 const server = new App();
-server.initializedRoutes([new UserRoute()]);
+server.initializedRoutes([new UserRoute(), new ProductRoute()]);
 server.listen();
