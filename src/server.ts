1e78d0d9bdb6b7186716a7f0bf14831e589ace73
thegodhope <103210115+thegodhope@users.noreply.github.com>
2023-10-08 14:01:17 +0100

    implemented create discount endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import App from './app';
 import UserRoute from './routes/user.route';
+import CreateDiscountRoute from './routes/createDiscount.route';
 
 const server = new App();
-server.initializedRoutes([new UserRoute()]);
+server.initializedRoutes([new UserRoute(), new CreateDiscountRoute()]);
 server.listen();
