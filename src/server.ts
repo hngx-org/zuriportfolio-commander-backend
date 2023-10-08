8c5ccb6f96cab20f2ce89dbd901bca365cb27679
aniebietafia <aniebietafia87@gmail.com>
2023-10-08 13:55:52 +0100

    feature: added get all orders & get all discounts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import App from './app';
 import UserRoute from './routes/user.route';
+import GetAllOrdersRoute from 'routes/get_all_orders.route';
+import GetAllDiscountRoute from 'routes/get_all_discount.route';
 
 const server = new App();
-server.initializedRoutes([new UserRoute()]);
+server.initializedRoutes([new UserRoute(), new GetAllOrdersRoute(), new GetAllDiscountRoute()]);
 server.listen();
