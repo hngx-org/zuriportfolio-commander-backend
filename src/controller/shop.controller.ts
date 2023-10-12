fc76cbde007cc5f726ff37267e7e885cf37d5051
aniebietafia <aniebietafia87@gmail.com>
2023-10-12 07:32:51 +0100

    update shop: controller for user to update shop

--- src/controller/shop.controller.ts
+++ src/controller/shop.controller.ts
@@ -41,4 +41,53 @@ export default class ShopController extends BaseController {
 
     this.success(res, '--shop/created', 'shop created', 200, created);
   }
+
+  async updateShop(req: Request, res: Response) {
+    // TODO: Fetch the shop id from req.params
+    const shopId = req.params.shop_id;
+
+    // TODO: Set the allowed fields to be updated
+    const allowedFields = ['name', 'policy_confirmation', 'restricted', 'admin_status', 'reviewed'];
+
+    const shop = await prisma.shop.findUnique({
+      where: {
+        id: shopId,
+      },
+    });
+
+    if (!shop) {
+      return this.error(res, '--shop/updateshop', 'shop not found', 404);
+    }
+
+    // check if fields are empty
+    if (Object.keys(req.body).length === 0) {
+      return this.error(res, '--shop/updateshop', 'no fields to update', 400);
+    }
+
+    // check if updates are allowed
+    const updates = Object.keys(req.body);
+    const isValidOperation = updates.every((update) => allowedFields.includes(update));
+    if (!isValidOperation) {
+      return this.error(res, '--shop/updateshop', 'invalid field(s)', 400);
+    }
+
+    // loop through the fields to be updated
+    for (const field in req.body) {
+      if (allowedFields.includes(field)) {
+        shop[field] = req.body[field];
+      }
+    }
+
+    // update shop
+    const updatedShop = await prisma.shop.update({
+      where: {
+        id: shopId,
+      },
+      data: {
+        ...shop,
+      },
+    });
+
+    this.success(res, '--shop/updateshop', 'shop updated successfully', 200, updatedShop);
+  }
 }
