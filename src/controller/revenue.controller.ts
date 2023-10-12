71b9dd9d63bfed0e7fdfce4efaa8b38be204ca88
Edidiva <edikanakpan5@gmail.com>
2023-10-12 08:03:51 +0100

    wip

--- src/controller/revenue.controller.ts
+++ src/controller/revenue.controller.ts
@@ -1,48 +1,48 @@
-// import { Request, Response } from 'express';
-// import BaseController from './base.controller';
-// import { PrismaClient } from '@prisma/client';
+import { Request, Response } from 'express';
+import BaseController from './base.controller';
+import { PrismaClient } from '@prisma/client';
 
-// const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
-// export default class RevenueController extends BaseController {
-//   constructor() {
-//     super();
-//   }
+export default class RevenueController extends BaseController {
+  constructor() {
+    super();
+  }
 
-//   async updateRevenue(req: Request, res: Response) {
-//     const { order_id } = req.params;
+  async updateRevenue(req: Request, res: Response) {
+    const { order_id } = req.params;
 
-//     const order = await prisma.order.findUnique({
-//       where: { id: order_id },
-//     });
+    const order = await prisma.order.findUnique({
+      where: { id: order_id },
+    });
 
-//     if (!order) {
-//       throw new Error('Order not found.');
-//     }
+    if (!order) {
+      throw new Error('Order not found.');
+    }
 
-//     if (order.status !== 'complete') {
-//       return res.status(400).json({ error: 'Order is not completed' });
-//     }
+    if (order.status !== 'complete') {
+      return res.status(400).json({ error: 'Order is not completed' });
+    }
 
-//     const revenueAmount = order.amount;
+    const revenueAmount = order.amount;
 
-//     const existingRevenue = await prisma.revenue.findFirst({
-//       where: {
-//         user_id: order.merchantId,
-//       },
-//     });
+    const existingRevenue = await prisma.revenue.findFirst({
+      where: {
+        user_id: order.merchantId,
+      },
+    });
 
-//     if (existingRevenue) {
-//       await prisma.revenue.update({
-//         where: {
-//           id: existingRevenue.id,
-//         },
-//         data: {
-//           amount: existingRevenue.amount + revenueAmount,
-//         },
-//       });
-//     }
+    if (existingRevenue) {
+      await prisma.revenue.update({
+        where: {
+          id: existingRevenue.id,
+        },
+        data: {
+          amount: existingRevenue.amount + revenueAmount,
+        },
+      });
+    }
 
-//     this.success(res, 'revenues', 'Revenue updated successfully', 200, existingRevenue);
-//   }
-// }
+    this.success(res, 'revenues', 'Revenue updated successfully', 200, existingRevenue);
+  }
+}
