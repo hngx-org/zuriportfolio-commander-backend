0757f22641b5eaa8a3c2e80836878747445dcb15
Oludayo <Oludayo@glinqiq.com>
2023-10-13 10:59:38 +0100

    Corrected the gatAllShops Controller

--- src/controller/shop.controller.ts
+++ src/controller/shop.controller.ts
@@ -76,7 +76,7 @@ export default class ShopController extends BaseController {
     if (shops.length > 0) {
       this.success(res, 'All shops', 'Shops have been listed successfully', 200, shops);
     } else {
-      this.success(res, '--shops-isEmpty', 'No Shop Found', 404, []);
+      this.success(res, '--shops-isEmpty', 'No Shop Found', 200, []);
     }
   }
   // Update existing shop controller
